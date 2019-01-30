ac47b2df70683228ebbc08653cd1368041aff7b7
Liang-hsiao <marsgt@qq.com>
2019-01-30 20:11:48 +0800

    docs: 更新"The menu"用例的注释

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -37,23 +37,15 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    // 抽屉菜单功能测试
     describe('The menu', function () {
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        // 测试菜单的初始状态是否是隐藏的
         it('element is hidden by default', function () {
             var isHidden = $('body').hasClass('menu-hidden');
             expect(isHidden).toBeTruthy();
         });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+         // 测试菜单点击后是否可以正常做显示/隐藏的切换
         it('changes visibility when the menu icon is clicked', function () {
             var menu = $('.menu-icon-link');
             
